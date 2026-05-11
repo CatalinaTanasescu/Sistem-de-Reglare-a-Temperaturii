530dd7c19d9367362d3ce2824ebe3e8eafd0ce6d
Pacuraru-Alex-Marius <alex.pacuraru24@gmail.com>
2026-05-11 17:08:51 +0300

    Added some slave behaviour to test the uvc + minor changes

--- tb/apb_uvc_tb.sv
+++ tb/apb_uvc_tb.sv
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps
+
 module tb_apb;
 
   import uvm_pkg::*;
@@ -5,7 +7,7 @@ module tb_apb;
   
   import apb_pkg::*;
   import apb_env_pkg::*;
-  import apb_pkg_tests::*;
+  import apb_tests_pkg::*;
 
   reg pclk;
   reg rst_n;
@@ -24,6 +26,22 @@ module tb_apb;
     .pclk  (pclk),
     .rst_n (rst_n)
   );
+
+  always_comb begin
+    if (!rst_n) begin
+      apb_if_inst.pready  = 1'b0;
+      apb_if_inst.prdata  = 32'h0;
+      apb_if_inst.pslverr = 1'b0;
+    end else if (apb_if_inst.psel && apb_if_inst.penable) begin
+      apb_if_inst.pready  = 1'b1;
+      apb_if_inst.pslverr = 1'b0;
+      apb_if_inst.prdata  = $urandom;
+    end else begin
+      apb_if_inst.pready  = 1'b0;
+      apb_if_inst.prdata  = 32'h0;
+      apb_if_inst.pslverr = 1'b0;
+    end
+  end
 
   initial begin
     uvm_config_db #(virtual apb_interface_dut)::set(null, "uvm_test_top.apb_environment", "apb_intf", apb_if_inst);
